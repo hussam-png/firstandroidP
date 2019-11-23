770b613a73177be0573b3949e7e593c62cd5a4aa
Hussam <hussamalhayek424@gmail.com>
2019-11-23 14:44:19 +0000

    initial commit

---
+++ app/src/main/java/NFZH/EXA/FTS/MessageActivity.java
@@ -0,0 +1,14 @@
+package NFZH.EXA.FTS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
